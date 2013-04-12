0b536222b55c0a058be7cf3fb3fdb5b451bd0e82
ajdm05 <angela.delgado@unitec.edu>
2013-04-12 11:56:07 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
     string name;
     int power_level;
     string model;
+    string favorite_food;
 
     cout << "Robot name? ";
     cin >> name;
@@ -16,13 +17,17 @@ int main()
     cin >> power_level;
     cout << "Robot model? ";
     cin >> model;
+    cout << "Robot Favorite Food? ";
+    cin >> favorite_food;
+
 
     cout << endl << "---Creating robot---" << endl;
-    Robot robot(name,power_level,model);
+    Robot robot(name,power_level,model,favorite_food);
     cout << "---Robot created---" << endl;
 
     cout << endl << "Robot name:" << robot.getName() << endl;
     cout << "Robot power level:" << robot.getPowerLevel() << endl;
     cout << "Robot model:" << robot.getModel() << endl;
+    cout << "Robot Favorite Food: " << robot.getFavoriteFood();
     return 0;
 }
