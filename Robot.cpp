ecae4ee001eaff88905b5b1ffc4140da46545008
ajdm05 <angela.delgado@unitec.edu>
2013-04-12 11:56:36 -0600

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -1,10 +1,11 @@
 #include "Robot.h"
 
-Robot::Robot(string name,int power_level,string model)
+Robot::Robot(string name,int power_level,string model,string favorite_food)
 {
     this->name=name;
     this->power_level=power_level;
     this->model=model;
+    this->favorite_food = favorite_food;
 }
 
 string Robot::getName()
@@ -20,6 +21,11 @@ int Robot::getPowerLevel()
 string Robot::getModel()
 {
     return model;
+}
+
+string Robot::getFavoriteFood()
+{
+    return favorite_food;
 }
 
 Robot::~Robot()
